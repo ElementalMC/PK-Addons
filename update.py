9acd846a1ebe710f1a24b3a7275caabe7c131d0f
Dooder07 <172064888+CozmycDev@users.noreply.github.com>
2024-09-14 04:14:50 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,16 +1,20 @@
-from bs4 import BeautifulSoup
 from datetime import datetime
+import re
 
 with open('index.html', 'r') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+    content = file.read()
 
-p_tag = soup.find('p')
+today = datetime.now().strftime('%b %d, %Y')
 
-if p_tag:
-    today = datetime.now().strftime('%b %d, %Y')
-    
-    new_text = f"This site is not owned or operated by ProjectKorra.<br>If you would like to see something added or removed, open a PR or issue on <a href=\"https://github.com/CozmycDev/PK-Addons\">the repo</a>.<br>Legend: <i class=\"fas fa-question-circle\"></i>=Unknown, <i class=\"fa-solid fa-x\"></i>=Dead Link <br>Last Updated: {today}"
-    p_tag.string = new_text
+p_content = f'''
+<p>
+  This site is not owned or operated by ProjectKorra.<br>If you would like to see something added or removed, open a PR or issue on <a href="https://github.com/CozmycDev/PK-Addons">the repo</a>.<br>Legend: <i class="fas fa-question-circle"></i>=Unknown, <i class="fa-solid fa-x"></i>=Dead Link<br>Last Updated: {today}
+</p>
+'''
 
-    with open('index.html', 'w') as file:
-        file.write(str(soup))
+pattern = re.compile(r'<p>.*?</p>', re.DOTALL)
+
+content = pattern.sub(p_content, content)
+
+with open('index.html', 'w') as file:
+    file.write(content)
